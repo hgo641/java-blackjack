5a9e997c505d1c51677d7def141453d27ac950c3
hgo641 <hgo641@gmail.com>
2023-03-10 08:57:29 +0900

    refactor: 접근제어자 수정

--- src/main/java/domain/user/User.java
+++ src/main/java/domain/user/User.java
@@ -5,24 +5,23 @@ import domain.card.Hand;
 import domain.card.Score;
 
 public abstract class User {
-
     protected final Hand hand = new Hand();
 
-    public void hit(final Card card) {
+    public final void hit(final Card card) {
         hand.addCard(card);
     }
 
-    public Hand getCards() {
+    public abstract boolean isHittable();
+
+    public abstract boolean isDealer();
+
+    public abstract boolean isPlayer();
+
+    public final Hand getCards() {
         return hand;
     }
 
-    abstract public boolean isHittable();
-
-    abstract public boolean isDealer();
-
-    abstract public boolean isPlayer();
-
-    public Score getScore() {
+    public final Score getScore() {
         return hand.getSumOfScores();
     }
 }
