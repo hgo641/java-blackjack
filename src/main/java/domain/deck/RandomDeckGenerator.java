2cfd2eda2b0cd8df56f9edb72e7fe8ca20242e05
hgo641 <hgo641@gmail.com>
2023-03-13 15:25:44 +0900

    git commit을 되돌리기 위한 commit

--- src/main/java/domain/deck/RandomDeckGenerator.java
+++ src/main/java/domain/deck/RandomDeckGenerator.java
@@ -4,12 +4,16 @@ import domain.card.Card;
 import domain.card.Denomination;
 import domain.card.Suits;
 import java.util.ArrayDeque;
+<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+=======
+import java.util.Deque;
+>>>>>>> parent of 4eabeaf (fix: 덱의 카드가 랜덤으로 정렬되게 수정)
 
 public class RandomDeckGenerator implements DeckGenerator {
-    private static void addCard(final List<Card> cards, final Suits suits) {
+    private static void addCard(final Deque<Card> cards, final Suits suits) {
         for (Denomination denomination : Denomination.values()) {
             cards.add(new Card(denomination, suits));
         }
@@ -17,11 +21,10 @@ public class RandomDeckGenerator implements DeckGenerator {
 
     @Override
     public Deck generateDeck() {
-        List<Card> cards = new ArrayList<>();
+        Deque<Card> cards = new ArrayDeque<>();
         for (Suits suits : Suits.values()) {
             addCard(cards, suits);
         }
-        Collections.shuffle(cards);
-        return new Deck(new ArrayDeque<>(cards));
+        return new Deck(cards);
     }
 }
--- src/test/java/domain/game/BlackjackTest.java
+++ src/test/java/domain/game/BlackjackTest.java
@@ -2,24 +2,14 @@ package domain.game;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-<<<<<<< HEAD:src/test/java/domain/game/BlackjackTest.java
-=======
-import domain.bettingMoney.BettingMoneyTable;
-import domain.bettingMoney.Monies;
-import domain.card.Card;
-import domain.card.Denomination;
-import domain.card.Suits;
->>>>>>> parent of d35bebf (feat: 참여자 수익 출력 기능 구현):src/test/java/domain/game/BlackJackTest.java
 import domain.deck.DefaultDeckGenerator;
 import domain.money.BettingMoneyTable;
 import domain.money.BettingMonies;
 import domain.name.Names;
-import domain.user.Dealer;
 import domain.user.Player;
 import domain.user.Users;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,6 +24,7 @@ public class BlackjackTest {
         BettingMoneyTable bettingMoneyTable = getDefaultBettingMoneyTable(users.getPlayers());
         blackJack = Blackjack.of(users, new DefaultDeckGenerator().generateDeck(), bettingMoneyTable);
     }
+
     @DisplayName("유저가 요청하면 카드를 하나 더 준다")
     @Test
     void giveCard_whenRequest() {
@@ -45,17 +36,13 @@ public class BlackjackTest {
         assertThat(player.getScore().getValue()).isGreaterThan(oldScore);
     }
 
-    private BettingMoneyTable getDefaultBettingMoneyTable(List<Player> players){
+    private BettingMoneyTable getDefaultBettingMoneyTable(List<Player> players) {
         return BettingMoneyTable.of(players, getDefaultMonies(players.size()));
     }
 
-<<<<<<< HEAD:src/test/java/domain/game/BlackjackTest.java
-    private BettingMonies getDefaultMonies(int size) {
-=======
-    private Monies getDefaultMonies(int size){
->>>>>>> parent of d35bebf (feat: 참여자 수익 출력 기능 구현):src/test/java/domain/game/BlackJackTest.java
+    private BettingMonies getDefaultMonies(final int size) {
         List<Integer> emptyMonies = new ArrayList<>();
-        for(int i=0; i<size; i++){
+        for (int i = 0; i < size; i++) {
             emptyMonies.add(0);
         }
         return BettingMonies.of(emptyMonies);
